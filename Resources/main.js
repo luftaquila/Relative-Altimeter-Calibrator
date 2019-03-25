a475d2e846b5fc6c275edf96057c2da096fb4ef6
LUFT-AQUILA <luftaquila@protonmail.ch>
2019-03-25 18:58:44 +0900

    v1.1.0 Improved

--- Resources/main.js
+++ Resources/main.js
@@ -7,6 +7,9 @@ $(function() {
     dataType: 'json',
     cache: false,
     success: function (response) {
+      $('#status').css('color', '#15be00');
+      $('#status').text('200 Ready.');
+      $('.hideBeforeLoad').css('display', 'inline');
       var str = '', html = new DOMParser().parseFromString(response.result, "text/html").getElementsByTagName('table')[0].getElementsByTagName('tr');
       for(var i = 0; i < html.length; i++) {
         data[i] = Object.values(html[i].getElementsByTagName('td'));
@@ -30,14 +33,14 @@ $(function() {
   });
   function updateAlt() {
     var gravityAcceleration = 9.806, coEff1 = 0.0065, coEff2 = 0.12, gasConst = 287.05;
-    var seaLvPressure = Number($('#seaLvPressure').val()), currentAirPressure = Number($('#currentAirPressure').val()), temp = Number($('#temp').val()), vaporPoint = Number($('#vaporPoint').val());
-    $('#altitude').val((Math.log(seaLvPressure / currentAirPressure) * (temp + 273.15 + vaporPoint * coEff2) / (gravityAcceleration / gasConst - coEff1 / 2 * Math.log(seaLvPressure / currentAirPressure))).toFixed(1));
+    var seaLvPressure = Number($('#seaLvPressure').text()), currentAirPressure = Number($('#currentAirPressure').val()), temp = Number($('#temp').text()), vaporPoint = Number($('#vaporPoint').val());
+    $('#altitude').text((Math.log(seaLvPressure / currentAirPressure) * (temp + 273.15 + vaporPoint * coEff2) / (gravityAcceleration / gasConst - coEff1 / 2 * Math.log(seaLvPressure / currentAirPressure))).toFixed(1));
   }
   function updateSpot() {
     data.forEach(function(val) {
       if($('#spot').val() == val[0]) {
-        $('#temp').val(val[1]);
-        $('#seaLvPressure').val(val[2]);
+        $('#temp').text(val[1]);
+        $('#seaLvPressure').text(val[2]);
       }
     });
   }
